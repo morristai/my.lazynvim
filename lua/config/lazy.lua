08189c1ae22128876c993d657072faf9a8fe9939
Morris Tai <morristai@apache.org>
2024-07-23 21:07:15 -0400

    chore: Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,7 +22,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.editor.trouble-v3" },
     { import = "lazyvim.plugins.extras.editor.leap" },
     -- import/override with your plugins
     { import = "plugins" },
